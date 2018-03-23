76a9f16245e4ec6e89de087c59529b99aff8d74d
NyanUnicorn <per1110@yahoo.com>
2018-03-23 22:14:52 +0100

    Initial commit

---
+++ src/com/IndividualSocket.java
@@ -0,0 +1,41 @@
+package com;
+
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class IndividualSocket {
+
+	public IndividualSocket(String client_ip, int socket_number) {
+		int number, skt;
+		boolean assigned = false;
+		skt = 1;
+//		while loop for socket
+		ServerSocket s1;
+		do {
+			try {
+				s1 = new ServerSocket(socket_number);
+				Socket ss=s1.accept();
+				Scanner sc = new Scanner(ss.getInputStream());
+				PrintStream p = new PrintStream(ss.getOutputStream());
+				number = sc.nextInt();
+				assigned = true;
+						
+			
+				do{
+
+					p.println(number*2);
+					number = sc.nextInt();			
+					
+				}while (number>0);
+				p.println(-1);
+				
+			}catch(Exception e) {
+				
+			}
+		}while(!assigned);
+		s1 = null;
+	}
+
+}
